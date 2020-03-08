8a61bb1e78f2aa2c9f2a1772143df944f6907c07
trv55 <61926693+trv55@users.noreply.github.com>
2020-03-08 17:00:02 +0900

    Add files via upload

---
+++ time.py
@@ -0,0 +1,17 @@
+import time
+from datetime import datetime
+from random import randint
+from winsound import Beep
+
+now = datetime.now()
+print ("%s시 %s분" %(now.hour, now.minute))
+
+i = randint(1,3)
+time.sleep(i)
+print(i)
+
+Beep(524,700)
+Beep(587,700)
+Beep(659,700)
+Beep(698,700)
+Beep(784,700)
