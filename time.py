5330a9c54b2bde76533c18fe930bc270843cdf15
trv55 <61926693+trv55@users.noreply.github.com>
2020-03-08 21:42:06 +0900

    Update time.py

--- time.py
+++ time.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python
+#-*- coding: euc-kr -*-
+
+import cgi
+import cgitb    #스크립트 오류 등의 에러발생시 브라우저에 에러 내용을 보여준다.
+cgitb.enable()
+print("Content-type: text/html\n")    #\n 을 안하면 Internal Server Error 발생!
+
+
 import time
 from datetime import datetime
 from random import randint
 from winsound import Beep
-import sys
-input=sys.argv[1]
-print(input)
 
 
 now = datetime.now()
